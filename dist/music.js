75a3edbad124d01a5805bb4b8c5f2d8621bd061c
pro bmpo <resrdsres@gmail.com>
2025-02-17 22:31:00 +0800

    Site updated: 2025-02-17 22:30:59

--- dist/music.js
+++ dist/music.js
@@ -1,22 +1,22 @@
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    autoplay: false,
-    loop: 'all',
-    volume: 0.7,
-    listFolded: true,
-    listMaxHeight: 60,
-    audio: [
+    container: document.getElementById('aplayer'), //播放器容器元素
+    mini: true //迷你模式
+    autoplay: true, //自动播放
+    theme: '#FADFA3', //主题色
+    loop: 'all', //音频循环播放, 可选值: 'all'全部循环, 'one'单曲循环, 'none'不循环
+    order: 'random', //音频循环顺序, 可选值: 'list'列表循环, 'random'随机循环
+    preload: 'auto', //预加载，可选值: 'none', 'metadata', 'auto'
+    volume: 0.7, //默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效
+    mutex: true, //互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器
+    listFolded: false, //列表默认折叠
+    listMaxHeight: 90, //列表最大高度
+    lrcType: 3, //歌词传递方式
+    audio: [ //音频信息,包含以下
         {
-            name: 'name1',
-            artist: 'artist1',
-            url: 'url1.mp3',
-            cover: 'cover1.jpg',
+            name: 'name1', //音频名称
+            artist: 'artist1', //音频艺术家
+            url: 'url1.mp3', //音频外链
         },
-        #{
-           # name: 'name2',
-           # artist: 'artist2',
-           # url: 'url2.mp3',
-          #  cover: 'cover2.jpg',
-       # }
+     
     ]
 });
