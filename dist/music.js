40dd83bd45d288893a11a259ab1bbdb463fa8501
pro bmpo <resrdsres@gmail.com>
2025-02-19 16:56:34 +0800

    Site updated: 2025-02-19 16:56:33

--- dist/music.js
+++ dist/music.js
@@ -4,11 +4,18 @@ const ap = new APlayer({
     autoplay: true,
     audio: [
         {
-            name: '歌曲名称',
-            artist: '艺术家',
+            name: '老人与海（管弦）',
+            artist: '未知艺术家',
+            url: '/music/laor.mp3', // 替换为你的本地音乐文件路径
+            //cover: '/images/cover.jpg', 替换为封面图片路径
+        },
+   {
+            name: 'ire_the_foggy_dew',
+            artist: '未知艺术家',
             url: '/music/ire_the_foggy_dew.mp3', // 替换为你的本地音乐文件路径
             //cover: '/images/cover.jpg', 替换为封面图片路径
-        }
+            
+        }，
         // 可以继续添加更多歌曲
     ]
 });
