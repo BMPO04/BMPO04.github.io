734178cced92e92951b11bb72dd100f984b70701
pro bmpo <resrdsres@gmail.com>
2025-02-19 17:09:49 +0800

    Site updated: 2025-02-19 17:09:48

--- dist/music.js
+++ dist/music.js
@@ -13,9 +13,8 @@ const ap = new APlayer({
             name: 'ire_the_foggy_dew',
             artist: '未知艺术家',
             url: '/music/ire_the_foggy_dew.mp3', // 替换为你的本地音乐文件路径
-            //cover: '/images/cover.jpg', 替换为封面图片路径
-            
-        }，
+            //cover: '/images/cover.jpg', 替换为封面图片路
+        }
         // 可以继续添加更多歌曲
     ]
 });
